569b337982c8944d5aa47311d7529fe7bf3e9b33
Nat Osten <uipoptart@gmail.com>
2014-05-07 15:28:50 -0700

    Initial Commit

---
+++ GPUImageTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GPUImageTest
+//
+//  Created by Nat Osten on 5/7/14.
+//  Copyright (c) 2014 Nat Osten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GPUImageTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GPUImageTest
+//
+//  Created by Nat Osten on 5/7/14.
+//  Copyright (c) 2014 Nat Osten. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
