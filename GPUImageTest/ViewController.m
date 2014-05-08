ae2f907f643b5116fd5cfe8f0fc2bf10212510b7
Nat Osten <uipoptart@gmail.com>
2014-05-07 17:48:02 -0700

    FEATURE: Display GPUImage on screen

--- GPUImageTest/ViewController.m
+++ GPUImageTest/ViewController.m
@@ -7,9 +7,35 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet GPUImageView *gpuImageView;
+@property (strong, nonatomic) GPUImageTiltShiftFilter *filter;
+
 @end
 
 @implementation ViewController
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self setupDisplayFiltering];
+}
+
+#pragma mark - Private API
+
+- (void)setupDisplayFiltering
+{
+    UIImage *image = [UIImage imageNamed:@"WID-small.jpg"];
+    
+    GPUImagePicture *sourcePicture = [[GPUImagePicture alloc] initWithImage:image];
+    GPUImageView *gpuImageView = self.gpuImageView;
+    GPUImageTiltShiftFilter *filter = [[GPUImageTiltShiftFilter alloc] init];
+   
+    [filter forceProcessingAtSizeRespectingAspectRatio:gpuImageView.sizeInPixels];
+    [sourcePicture addTarget:filter];
+    [filter addTarget:gpuImageView];
+    
+    [sourcePicture processImage];
+}
 
 @end
